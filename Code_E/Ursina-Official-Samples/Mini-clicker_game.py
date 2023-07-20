9714a4ae745f6750f4e6d5602f2b43246c5c478b
Andreius_14 <Andreius_14@outlook.com>
2023-07-19 21:13:24 -0500

    Nodemon

--- Code_E/Ursina-Official-Samples/Mini-clicker_game.py
+++ Code_E/Ursina-Official-Samples/Mini-clicker_game.py
@@ -6,15 +6,13 @@ you earn gold for every click
 when you have enough gold you can unlock new nodes to automatically generate gold!
 '''
 
-
-
 from ursina import *
 
 app = Ursina()
 window.color = color._20
 
 gold = 0
-counter = Text(text='0', y=.25, z=-1, scale=2, origin=(0,0), background=True)
+counter = Text(text='0', y=.25, z=-1, scale=4, origin=(0,0), background=True)
 button = Button(text='+', color=color.azure, scale= .125)
 
 def button_click():
@@ -22,10 +20,8 @@ def button_click():
     gold += 1
     counter.text = str(gold)
 
+# Al accionar el Evento 
 button.on_click = button_click
-
-
-
 button_2 = Button(cost=10, x=.2, scale=.125, color=color.dark_gray, disabled=True)
 button_2.tooltip = Tooltip(f'<gold>Gold Generator\n<default>Earn 1 gold every second.\nCosts {button_2.cost} gold.')
 
@@ -36,18 +32,16 @@ def buy_auto_gold():
         counter.text = str(gold)
         invoke(auto_generate_gold, 1, 1)
 
+# Al accionar el Evento 
 button_2.on_click = buy_auto_gold
-
-
 
 def auto_generate_gold(value=1, interval=1):
     global gold
-    gold += 1
+    gold += 100
     counter.text = str(gold)
     button_2.animate_scale(.125 * 1.1, duration=.1)
     button_2.animate_scale(.125, duration=.1, delay=.1)
     invoke(auto_generate_gold, value, delay=interval)
-
 
 def update():
     global gold
